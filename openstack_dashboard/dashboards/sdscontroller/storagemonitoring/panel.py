64d301c2ae93d6ff3d4c75575776f83e9abd54e4
genis ortiz <genis.ortiz.gomis@gmail.com>
2015-12-02 13:48:20 +0100

    Added initial graphs, system and swift monitoring

--- openstack_dashboard/dashboards/sdscontroller/storagemonitoring/panel.py
+++ openstack_dashboard/dashboards/sdscontroller/storagemonitoring/panel.py
@@ -15,6 +15,7 @@ from django.utils.translation import ugettext_lazy as _
 import horizon
 from openstack_dashboard.dashboards.sdscontroller import dashboard
 
+
 class Storagemonitoring(horizon.Panel):
     name = _("Storage Monitoring")
     slug = "storagemonitoring"
