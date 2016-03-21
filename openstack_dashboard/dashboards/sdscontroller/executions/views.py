b6eb6ce521b769efe59991b3658b9263f5a9bed0
Marco Milanesio <milanesio.marco@gmail.com>
2016-03-21 17:29:53 +0100

    removed terminateexec view
    Signed-off-by: Marco Milanesio <milanesio.marco@gmail.com>

--- openstack_dashboard/dashboards/sdscontroller/executions/views.py
+++ openstack_dashboard/dashboards/sdscontroller/executions/views.py
@@ -9,19 +9,9 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from django.core.urlresolvers import reverse
-from django.core.urlresolvers import reverse_lazy
-from django.utils.translation import ugettext_lazy as _
 
 from horizon import tabs
-from horizon import exceptions
-from horizon import forms
 
-from horizon.utils import memoized
-
-from openstack_dashboard import api
-
-from openstack_dashboard.dashboards.sdscontroller.executions import forms as project_forms
 from openstack_dashboard.dashboards.sdscontroller.executions import tabs as mydashboard_tabs
 
 
@@ -31,26 +21,4 @@ class IndexView(tabs.TabbedTableView):
 
     def get_data(self, request, context, *args, **kwargs):
         # Add data to the context here...
-        return context
-
-
-class TerminateView(forms.ModalFormView):
-    form_class = project_forms.TerminateZoeExecution
-    template_name = 'sdscontroller/executions/terminate.html'
-    success_url = reverse_lazy("horizon:sdscontroller:executions:index")
-    modal_id = "terminate_zoe_execution"
-    modal_header = _("Terminate Zoe Execution")
-    submit_label = _("Terminate Zoe Execution")
-    submit_url = "horizon:sdscontroller:executions:terminate"
-
-    def get_initial(self):
-        return {"instance_id": self.kwargs["instance_id"]}
-
-    def get_context_data(self, **kwargs):
-        context = super(TerminateView, self).get_context_data(**kwargs)
-        instance_id = self.kwargs['instance_id']
-        context['instance_id'] = instance_id
-        #context['instance'] = self.get_object()
-        context['instance'] = None
-        context['submit_url'] = reverse(self.submit_url, args=[instance_id])
         return context
