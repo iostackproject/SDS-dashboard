f862832e8b84d70be4c278b6c98a6ce425aaad39
Marco Milanesio <milanesio.marco@gmail.com>
2016-03-22 16:16:29 +0100

    Signed-off-by: Marco Milanesio <milanesio.marco@gmail.com>

--- openstack_dashboard/dashboards/sdscontroller/zoemonitoring/tabs.py
+++ openstack_dashboard/dashboards/sdscontroller/zoemonitoring/tabs.py
@@ -3,19 +3,20 @@ from django.utils.translation import ugettext_lazy as _
 from horizon import tabs
 
 
-# class ProxyTab(tabs.TableTab):
-#     name = _("Proxy System")
-#     slug = "proxy_plots"
-#     template_name = "horizon/sdscontroller/storagemonitoring/system/_system_plots2.html"
-
-
 class ZoeAppTab(tabs.TableTab):
     name = _("Zoe App")
+    slug = "zoe_app_plots"
+    template_name = "horizon/sdscontroller/zoemonitoring/_app_plots.html"
+
+
+class ZoeStatusTab(tabs.TableTab):
+    name = _("Zoe Status")
     slug = "zoe_plots"
     template_name = "horizon/sdscontroller/zoemonitoring/_zoe_plots.html"
 
+
 class MypanelTabs(tabs.TabGroup):
     slug = "zoe_monitoring_tabs"
-    tabs = (ZoeAppTab,) # ZoeStatusTab,)
+    tabs = (ZoeAppTab, ZoeStatusTab,)
     sticky = True
 
