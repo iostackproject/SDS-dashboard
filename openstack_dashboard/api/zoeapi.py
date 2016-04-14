3f88a8d0b8d5a07d1fa3409f7d4435a384f0d7b7
marcomilanesio <milanesio.marco@gmail.com>
2016-04-14 11:54:36 +0200

    switched to call branch iostack in zoe repo
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- openstack_dashboard/api/zoeapi.py
+++ openstack_dashboard/api/zoeapi.py
@@ -2,7 +2,9 @@ import requests
 from horizon.utils.memoized import memoized  # noqa
 import json
 from zoe_lib.executions import ZoeExecutionsAPI
-from zoe_lib.predefined_frameworks import jupyter_spark, spark, openmpi
+from zoe_lib.predefined_apps import spark_interactive
+from zoe_lib.predefined_apps import wordcount_iostack
+from zoe_lib.predefined_apps import openmpi_iostack
 
 # TODO: Take parameters from a config file
 URL_BASIC = "http://127.0.0.1:8000"
@@ -49,13 +51,13 @@ def new_execution(request, exec_name, app_name):
     exec_api = ZoeExecutionsAPI(cfg['ZOE_URL'], cfg['ZOE_USER'], cfg['ZOE_PWD'])
     if app_name == 'ipython':
         print("Starting ipython notebook Zoe execution: ", exec_name)
-        app_descr = jupyter_spark.spark_jupyter_notebook_app()
+        app_descr = spark_interactive.spark_jupyter_notebook_app()
     elif app_name == 'spark':
         print("Starting Spark Cluster Zoe execution: ", exec_name)
-        app_descr = spark.spark_submit_service()
+        app_descr = wordcount_iostack.iostack_wordcount_app()
     elif app_name == 'mpi':
         print("Starting MPI Zoe execution: ", exec_name)
-        app_descr = openmpi.openmpi_mpirun_service()
+        app_descr = openmpi_iostack.openmpi_hello_app(name="mpihello")
     else:
         print("App not supported.")
         return
