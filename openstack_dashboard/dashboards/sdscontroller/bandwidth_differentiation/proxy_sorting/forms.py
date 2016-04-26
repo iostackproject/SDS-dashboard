bbab13306d43d434947bd4d158e0f5c49764477e
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-04-26 16:22:16 +0200

    Added API call to create proxy sorting.

--- openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/proxy_sorting/forms.py
+++ openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/proxy_sorting/forms.py
@@ -9,26 +9,26 @@ from openstack_dashboard.dashboards.sdscontroller import exceptions as sdsexcept
 
 
 class CreateSortedMethod(forms.SelfHandlingForm):
-    sorted_nodes_method = forms.CharField(max_length=255,
-                                          label=_("Sorted Nodes Method:"),
-                                          help_text=_("The sorted_method name."),
-                                          widget=forms.TextInput(
-                                              attrs={"ng-model": "sorted_nodes_method", "not-blank": ""}
-                                          ))
+    name = forms.CharField(max_length=255,
+                           label=_("Sorted Nodes Method:"),
+                           help_text=_("The sorted_method name."),
+                           widget=forms.TextInput(
+                               attrs={"ng-model": "sorted_nodes_method", "not-blank": ""}
+                           ))
 
-    sorted_nodes_criterion = forms.CharField(max_length=255,
-                                             label=_("Sorted Nodes Criterion:"),
-                                             help_text=_("ascending or descending ."),
-                                             widget=forms.TextInput(
-                                                 attrs={"ng-model": "sorted_nodes_criterion", "not-blank": ""}
-                                             ))
+    criterion = forms.CharField(max_length=255,
+                                label=_("Sorted Nodes Criterion:"),
+                                help_text=_("ascending or descending ."),
+                                widget=forms.TextInput(
+                                    attrs={"ng-model": "sorted_nodes_criterion", "not-blank": ""}
+                                ))
 
     def __init__(self, request, *args, **kwargs):
         super(CreateSortedMethod, self).__init__(request, *args, **kwargs)
 
     def handle(self, request, data):
         try:
-            response = api.set_sort_nodes(request, data)
+            response = api.bw_add_sort_method(request, data)
             if 200 <= response.status_code < 300:
                 messages.success(request, _('Successfully sorted method creation.'))
                 return data
