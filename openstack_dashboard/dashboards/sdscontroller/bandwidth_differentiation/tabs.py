5da7c2b60e55640ffc0e2b975926688f7b7ce240
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-04-26 16:23:49 +0200

    Added an API call to get all SLAs and Proxy Sorting.

--- openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/tabs.py
+++ openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/tabs.py
@@ -4,12 +4,14 @@ from django.utils.translation import ugettext_lazy as _
 
 from horizon import exceptions
 from horizon import tabs
+from openstack_dashboard.api import sds_controller as api
+from openstack_dashboard.dashboards.sdscontroller import exceptions as sdsexception
+from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.proxy_sorting import models as proxy_sorting_models
 from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.proxy_sorting import tables as proxy_sorting_tables
 from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.slas import models as slas_models
 from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.slas import tables as slas_tables
 
 
-# TODO: Check this class, we need an API call.
 class SLAsTab(tabs.TableTab):
     table_classes = (slas_tables.SLAsTable,)
     name = _("SLAs")
@@ -18,13 +20,12 @@ class SLAsTab(tabs.TableTab):
 
     def get_slas_data(self):
         try:
-            strobj = "[]"
-            # response = api.bw_list_slas(self.request)
-            # if 200 <= response.status_code < 300:
-            #     strobj = response.text
-            # else:
-            #     error_message = 'Unable to get instances.'
-            #     raise sdsexception.SdsException(error_message)
+            response = api.bw_get_all_slas(self.request)
+            if 200 <= response.status_code < 300:
+                strobj = response.text
+            else:
+                error_message = 'Unable to get instances.'
+                raise sdsexception.SdsException(error_message)
         except Exception as e:
             strobj = "[]"
             exceptions.handle(self.request, _(e.message))
@@ -36,7 +37,6 @@ class SLAsTab(tabs.TableTab):
         return ret
 
 
-# TODO: Check this class, get_proxy_sorting_data method is not working yet.
 class ProxySortingTab(tabs.TableTab):
     table_classes = (proxy_sorting_tables.ProxySortingTable,)
     name = _("Proxy Sorting")
@@ -44,7 +44,22 @@ class ProxySortingTab(tabs.TableTab):
     template_name = ("horizon/common/_detail_table.html")
 
     def get_proxy_sorting_data(self):
-        return []
+        try:
+            response = api.bw_get_all_sort_method(self.request)
+            if 200 <= response.status_code < 300:
+                strobj = response.text
+            else:
+                error_message = 'Unable to get instances.'
+                raise sdsexception.SdsException(error_message)
+        except Exception as e:
+            strobj = "[]"
+            exceptions.handle(self.request, _(e.message))
+
+        instances = json.loads(strobj)
+        ret = []
+        for inst in instances:
+            ret.append(proxy_sorting_models.ProxySorting(inst['id'], inst['name'], inst['criterion']))
+        return ret
 
 
 class MypanelTabs(tabs.TabGroup):
