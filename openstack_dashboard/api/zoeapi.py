ecfede53305f073c6ec5b29d6df2780d04a1ddeb
marcomilanesio <milanesio.marco@gmail.com>
2016-06-06 17:21:58 +0200

    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- openstack_dashboard/api/zoeapi.py
+++ openstack_dashboard/api/zoeapi.py
@@ -46,9 +46,7 @@ def get_execution_details(exec_id):
     exec_api = ZoeExecutionsAPI(cfg['ZOE_URL'], cfg['ZOE_USER'], cfg['ZOE_PWD'])
     cont_api = ZoeServiceAPI(cfg['ZOE_URL'], cfg['ZOE_USER'], cfg['ZOE_PWD'])
     exec_details = exec_api.execution_get(exec_id)
-    print("*******\n")
-    print(type(exec_details))
-    print("*******\n")
+    print("EXECUTION DETAILS:", type(exec_details))
     service_details = {}
     for c_id in exec_details['services']:
         c = cont_api.get(c_id)
