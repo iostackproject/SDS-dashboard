ee676a9d738c7157db882fde23cc9202d5059b50
marcomilanesio <milanesio.marco@gmail.com>
2016-06-08 17:17:48 +0200

    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- openstack_dashboard/api/zoeapi.py
+++ openstack_dashboard/api/zoeapi.py
@@ -57,25 +57,6 @@ def get_user_info(exec_details):
     name = owner['owner']
     gateway = owner['gateway_urls'][0]
     return name, gateway
-    #exec_api = ZoeExecutionsAPI(ZOE_URL, ZOE_USER, ZOE_PWD)
-    #user_api = ZoeUserAPI(ZOE_URL, ZOE_USER, ZOE_PWD)
-    #data = exec_api.list()
-    #print("zoe api: get_user_info. data = {}".format([(u['owner'], u['id'], u['name']) for u in data]))
-    #try:
-    #    execution = [e for e in data if e['id'] == exec_id][0]
-    #    print("zoe api: get_user_info. execution = {}".format(execution['id']))
-    #except Exception as e:
-    #    print("zoe api: get_user_info: no execution found {}".format(exec_id))
-    #    print("zoe api: get_user_info: no execution found {}".format(e))
-    #    execution = None
-    #if execution:
-    #    owner = user_api.get(execution['owner'])
-    #    print("zoe api: get_user_info. owner = {}".format(owner))
-    #    name = owner['owner']
-    #    gateway = owner['gateway_urls'][0]
-    #    return name, gateway
-    #else:
-    #    return '', ''  #FIXME return None?
 
 
 def get_execution_details(exec_id):
@@ -90,7 +71,6 @@ def get_execution_details(exec_id):
         cont_api = ZoeServiceAPI(ZOE_URL, ZOE_USER, ZOE_PWD)
         exec_details = exec_api.execution_get(exec_id)
         owner, gateway = get_user_info(exec_details)
-        #owner, gateway = get_user_info(exec_id, exec_details)
         service_details = []
         for c_id in exec_details['services']:
             c = cont_api.get(c_id)
