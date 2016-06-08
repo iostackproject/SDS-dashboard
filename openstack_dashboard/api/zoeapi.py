d69d67b12b95fdd161d01f1bb104142c7dc3afd4
marcomilanesio <milanesio.marco@gmail.com>
2016-06-08 11:13:38 +0200

    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- openstack_dashboard/api/zoeapi.py
+++ openstack_dashboard/api/zoeapi.py
@@ -76,7 +76,9 @@ def get_execution_details(exec_id):
     exec_api = ZoeExecutionsAPI(ZOE_URL, ZOE_USER, ZOE_PWD)
     cont_api = ZoeServiceAPI(ZOE_URL, ZOE_USER, ZOE_PWD)
     owner, gateway = get_user_info(exec_id)
+    print("zoe api: call to get_user_info {} - {}".format(owner, gateway))
     exec_details = exec_api.execution_get(exec_id)
+    exec_details.update({'owner': owner, 'gateway': gateway})
     service_details = []
     for c_id in exec_details['services']:
         c = cont_api.get(c_id)
@@ -88,7 +90,7 @@ def get_execution_details(exec_id):
             url = "{}://{}:{}{}".format(p['protocol'], ip, p['port_number'], p['path'])
             tmp['details'] = {'name': p['name'], 'url': url}
         service_details.append(tmp)
-    exec_details.update({'service_details': service_details, 'owner': owner, 'gateway': gateway})
+    exec_details.update({'service_details': service_details})
     return exec_details
 
 
