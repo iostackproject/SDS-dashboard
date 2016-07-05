e5444a84147b0f4232eaa519e6df79724ab276e5
Marsqui <marc.siquier@bsc.es>
2016-07-05 09:24:20 +0200

    Switch swift version button

--- openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/urls.py
+++ openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/urls.py
@@ -5,9 +5,11 @@ from django.conf.urls import url
 from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation import views
 from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.proxy_sorting import urls as proxy_sorting_urls
 from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.slas import urls as slas_urls
+from openstack_dashboard.dashboards.sdscontroller.bandwidth_differentiation.change_swift import urls as change_swift_urls
 
 urlpatterns = patterns('',
                        url(r'^$', views.IndexView.as_view(), name='index'),
                        url(r'slas/', include(slas_urls, namespace="slas")),
                        url(r'proxy_sorting/', include(proxy_sorting_urls, namespace="proxy_sorting")),
+                       url(r'change_swift/', include(change_swift_urls, namespace="change_swift")),
                        )
