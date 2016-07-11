02a0fcfe099515d653da4f2d99d0f61d3b261168
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-07-11 08:56:25 +0200

    Modified tabs and urls to point to the new policy table.

--- openstack_dashboard/dashboards/sdscontroller/storagepolicies/tabs.py
+++ openstack_dashboard/dashboards/sdscontroller/storagepolicies/tabs.py
@@ -5,20 +5,17 @@ from django.utils.translation import ugettext_lazy as _
 from horizon import exceptions
 from horizon import tabs
 from openstack_dashboard.api import sds_controller as api
-from openstack_dashboard.dashboards.sdscontroller.storagepolicies.dynamic_policies import models as dynamic_policies_models
-from openstack_dashboard.dashboards.sdscontroller.storagepolicies.dynamic_policies import tables as dynamic_policies_tables
 from openstack_dashboard.dashboards.sdscontroller.storagepolicies.metrics import models as metrics_models
 from openstack_dashboard.dashboards.sdscontroller.storagepolicies.metrics import tables as metrics_tables
-from openstack_dashboard.dashboards.sdscontroller.storagepolicies.static_policies import models as static_policies_models
-from openstack_dashboard.dashboards.sdscontroller.storagepolicies.static_policies import tables as static_policies_tables
+from openstack_dashboard.dashboards.sdscontroller.storagepolicies.policies import models as policies_models
+from openstack_dashboard.dashboards.sdscontroller.storagepolicies.policies import tables as policies_tables
 
 
-class StaticPolicyTab(tabs.TableTab):
-    name = _("Static Policies")
-    slug = "static_policies_tab"
-    table_classes = (static_policies_tables.PoliciesTable,)
-    template_name = ("horizon/common/_detail_table.html")
-    preload = False
+class Policies(tabs.TableTab):
+    table_classes = (policies_tables.StaticPoliciesTable, policies_tables.DynamicPoliciesTable,)
+    name = _("Policies")
+    slug = "policies_table"
+    template_name = "sdscontroller/storagepolicies/policies/_detail.html"
 
     def get_static_policies_data(self):
         try:
@@ -35,16 +32,8 @@ class StaticPolicyTab(tabs.TableTab):
         instances = json.loads(strobj)
         ret = []
         for inst in instances:
-            ret.append(static_policies_models.Policy(inst['id'], inst['target_id'], inst['target_name'], inst['filter_name'], inst['object_type'], inst['object_size'], inst['execution_server'], inst['execution_server_reverse'], inst['execution_order'], inst['params']))
+            ret.append(policies_models.StaticPolicy(inst['id'], inst['target_id'], inst['target_name'], inst['filter_name'], inst['object_type'], inst['object_size'], inst['execution_server'], inst['execution_server_reverse'], inst['execution_order'], inst['params']))
         return ret
-
-
-class DynamicPolicyTab(tabs.TableTab):
-    name = _("Dynamic Policies")
-    slug = "dynamic_policies_tab"
-    table_classes = (dynamic_policies_tables.PoliciesTable,)
-    template_name = ("horizon/common/_detail_table.html")
-    preload = False
 
     def get_dynamic_policies_data(self):
         try:
@@ -61,7 +50,7 @@ class DynamicPolicyTab(tabs.TableTab):
         instances = json.loads(strobj)
         ret = []
         for inst in instances:
-            ret.append(dynamic_policies_models.Policy(inst['id'], inst['policy'], inst['policy_location'], inst['alive']))
+            ret.append(policies_models.DynamicPolicy(inst['id'], inst['policy'], inst['policy_location'], inst['alive']))
         return ret
 
 
@@ -93,5 +82,5 @@ class MetricTab(tabs.TableTab):
 
 class PoliciesGroupTabs(tabs.TabGroup):
     slug = "policies_group_tabs"
-    tabs = (StaticPolicyTab, DynamicPolicyTab, MetricTab)
+    tabs = (Policies, MetricTab,)
     sticky = True
