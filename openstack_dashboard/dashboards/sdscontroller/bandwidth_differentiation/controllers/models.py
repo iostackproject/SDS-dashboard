3d10b18731084868c2724392cd4c19464ceda660
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-12-21 18:22:58 +0100

    Added GET, PUT and Replication tables of bw_controllers.

---
+++ openstack_dashboard/dashboards/sdscontroller/bandwidth_differentiation/controllers/models.py
@@ -0,0 +1,9 @@
+class Controller:
+    """
+        Controllers class models.
+    """
+
+    def __init__(self, controller_id, name, enabled):
+        self.id = controller_id
+        self.name = name
+        self.enabled = enabled
